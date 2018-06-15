5d7603f1c9517b2fdec70027ecf40b58956ab0fc
poyu Xu <jasugun0000@gmail.com>
2018-06-16 02:00:52 +0800

    update

--- setting.sh
+++ setting.sh
@@ -1,4 +1,4 @@
 cp -r .vim/ ~/ &&\
 cp .vimrc ~/ &&\
-cp setting.json ~/.config/Code/User/ &&\
-cp php.json ~/.config/Code/User/snippets/
+cp ./setting.json ~/.config/Code/User/ &&\
+cp ./php.json ~/.config/Code/User/snippets/
