3ac8dc1eefbbf6f0678bef86c5899ed0458fd4cd
poyu Xu <jasugun0000@gmail.com>
2018-06-16 01:28:51 +0800

    update composer code

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install software-properties-common -y &&\
 sudo add-apt-repository ppa:ondrej/php -y && \
 sudo apt-get update && \
 sudo apt-get install php7.2 -y && \
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && php composer-setup.php && php -r "unlink('composer-setup.php');" && mv composer.phar /usr/local/bin/composer && apt-get -y install git zip unzip && \
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && php composer-setup.php && php -r "unlink('composer-setup.php');" && sudo mv composer.phar /usr/local/bin/composer && apt-get -y install git zip unzip && \
 composer global require friendsofphp/php-cs-fixer &&\
 composer global require "squizlabs/php_codesniffer=*" && \
 sudo apt-get remove docker docker-engine docker.io -y && \
